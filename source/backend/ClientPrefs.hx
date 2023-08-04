b77b1fc41c54d5495d5bb57a0eab1e4a3264c767
Sonicgamer2000Alt <141368813+Sonicgamer2000Alt@users.noreply.github.com>
2023-08-04 09:12:28 -0400

    Normal Note Colors are now like the corruption

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -26,10 +26,10 @@ class SaveVariables {
 	public var hideHud:Bool = false;
 	public var noteOffset:Int = 0;
 	public var arrowRGB:Array<Array<FlxColor>> = [
-		[0xFFC24B99, 0xFFFFFFFF, 0xFF3C1F56],
-		[0xFF00FFFF, 0xFFFFFFFF, 0xFF1542B7],
-		[0xFF12FA05, 0xFFFFFFFF, 0xFF0A4447],
-		[0xFFF9393F, 0xFFFFFFFF, 0xFF651038]];
+		[0xFF87A3AD, 0xFFFFFFFF, 0xFF87A3AD],
+		[0xFF87A3AD, 0xFFFFFFFF, 0xFF87A3AD],
+		[0xFF87A3AD, 0xFFFFFFFF, 0xFF87A3AD],
+		[0xFF87A3AD, 0xFFFFFFFF, 0xFF87A3AD]];
 	public var arrowRGBPixel:Array<Array<FlxColor>> = [
 		[0xFFE276FF, 0xFFFFF9FF, 0xFF60008D],
 		[0xFF3DCAFF, 0xFFF4FFFF, 0xFF003060],
